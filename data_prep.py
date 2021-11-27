97aa99e85fe95bdabff62605cfd55e65fdeb82f5
Ahmad Qadri <arqchicago@gmail.com>
2021-11-26 23:45:58 -0500

    adding support for validation set in mnist class

--- data_prep.py
+++ data_prep.py
@@ -13,16 +13,25 @@ class mnist:
         # - training_set is a tuple (60000 images, 60000 labels)
         # - testing_set is a tuple (10000 images, 10000 labels)
         self.training_set, self.testing_set = tf.keras.datasets.mnist.load_data(path="mnist.npz")
+
         #print(f'training set (x): {len(self.training_set[0])}  training set (y): {len(self.training_set[1])}')
         #print(f'testing set (x): {len(self.testing_set[0])}    testing set (y): {len(self.testing_set[1])}')
 
     def get_raw_datasets(self):
         return self.training_set, self.testing_set
         
-    def get_vectorized_datasets(self):
+    def get_vectorized_datasets(self, validation_data_pct=0.0):
         training_x = [np.reshape(i, (784,1)) for i in self.training_set[0]]
         testing_x = [np.reshape(i, (784,1)) for i in self.testing_set[0]]
         
+        if validation_data_pct>0.0:
+            random.shuffle(testing_x)
+            n_validation_x = int(round(len(testing_x) * validation_data_pct,0))
+            n_testing_x = len(testing_x) - n_validation_x
+            
+            validation_x = testing_x[0:n_validation_x]
+            testing_x = testing_x[n_validation_x:]          
+            
         training_y = []
         for i in self.training_set[1]:
             y_array = np.zeros((10,1))
@@ -36,13 +45,13 @@ class mnist:
             y_array[i] = 1.0
             testing_y.append(y_array)
         
-        testing_data = list(zip(testing_x, testing_y))  #python2 returned a list but python3 returns an iterable object
-        return training_data, testing_data
+        testing_data = list(zip(testing_x, testing_y))  #python2 returned a list but python3 returns an iterable object        
         
+        return training_data, testing_data
+
 '''
 if __name__ == "__main__":
+    get_val_data = 0.20
     mnist_data = mnist()
-    training_data, testing_set = mnist_data.get_vectorized_datasets()
-    print(training_data[0])
-    print(testing_set[0])
+    training_data, testing_set = mnist_data.get_vectorized_datasets(get_val_data)
 '''
